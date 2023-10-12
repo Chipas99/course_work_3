1d02004c1e64cc9c3dbd4a55e34350ebf08e6171
egorn <egornich99@mail.ru>
2023-10-13 00:57:32 +0300

    поправил курсовую

--- src/main.py
+++ src/main.py
@@ -1,21 +1,13 @@
 import datetime
 import json
-from .utils import mask_card, mask_account
+from src.utils import filter_and_sorting, prepare_user_message
+
 
 def display_operations():
-    with open('data/operations.json') as file:
-        operations = json.load(file)
+    with open('../operations.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
 
-    operations = [op for op in operations if op['state'] == 'EXECUTED']
+    items = filter_and_sorting(data)
 
-    for operation in operations[-5:][::-1]:
-        transfer_date = datetime.datetime.strptime(operation['date'], "%d/%m/%Y %H:%M:%S")
-        description = operation['description']
-        from_acct = mask_card(operation['from']) if operation['from'].count(' ') == 2 else mask_account(operation['from'])
-        to_acct = mask_account(operation['to'])
-        amount = operation['operationAmount']
-
-        print(f'{transfer_date}\n{description}\n{from_acct} -> {to_acct}\n{amount}\n')
-
-if __name__ == "__main__":
-    display_operations()
+    for i in range(5):
+        print(prepare_user_message(items[i]))
